fbc5cf862316bc5763c4211cf41890960153b74a
Slav Lalev <slalev57@me.com>
2019-09-24 18:05:08 +0000

    Spinner doing a full spin

---
+++ spinner1.js
@@ -0,0 +1,35 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r\|   '); 
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r\/   '); 
+},1200);
+
+setTimeout(() => {
+  process.stdout.write('\r\-   '); 
+},1400);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+},1600);
+
+setTimeout(() => {
+  process.stdout.write('\r\|   '); 
+},1800);
